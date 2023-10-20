0574b2442e216fc3a299c320b609373f81caec89
ElijahSadorra <86716797+ElijahSadorra@users.noreply.github.com>
2023-10-20 16:17:15 +0100

    Added an interface

---
+++ main.py
@@ -0,0 +1,11 @@
+from gameObject import GameObject
+
+# The main program starts here
+if __name__ == "__main__":
+    
+    # Creates an instance game object
+    gameObject = GameObject()
+    
+    # Starts game object loop
+    gameObject.start()
+    
